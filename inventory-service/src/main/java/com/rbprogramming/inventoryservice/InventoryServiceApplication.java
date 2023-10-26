129631e6ac314162ac3c001e32f4170e456d904c
RobsonTrasel <robson.trasel@gmail.com>
2023-10-26 00:27:39 -0300

    chore(main): run a clr loadData to seed the database with default values for inventory
    Signed-off-by: RobsonTrasel <robson.trasel@gmail.com>

--- inventory-service/src/main/java/com/rbprogramming/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/rbprogramming/inventoryservice/InventoryServiceApplication.java
@@ -1,7 +1,11 @@
 package com.rbprogramming.inventoryservice;
 
+import com.rbprogramming.inventoryservice.model.Inventory;
+import com.rbprogramming.inventoryservice.repository.InventoryRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
@@ -10,4 +14,19 @@ public class InventoryServiceApplication {
 		SpringApplication.run(InventoryServiceApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner loadData(InventoryRepository inventoryRepository) {
+		return args -> {
+			Inventory inventory = new Inventory();
+			inventory.setSkuCode("iphone_15");
+			inventory.setQuantity(100);
+
+			Inventory inventory1 = new Inventory();
+			inventory1.setSkuCode("iphone_15_blue");
+			inventory1.setQuantity(0);
+
+			inventoryRepository.save(inventory1);
+			inventoryRepository.save(inventory);
+		};
+	}
 }
