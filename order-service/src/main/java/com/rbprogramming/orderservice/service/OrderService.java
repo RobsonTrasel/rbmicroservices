ae03dc251c1f7f81c99dd6c1fd1086ec87509721
RobsonTrasel <robson.trasel@gmail.com>
2023-10-25 23:15:20 -0300

    feat(service): create new OrderService for logical purposes
    Signed-off-by: RobsonTrasel <robson.trasel@gmail.com>

---
+++ order-service/src/main/java/com/rbprogramming/orderservice/service/OrderService.java
@@ -0,0 +1,39 @@
+package com.rbprogramming.orderservice.service;
+
+import com.rbprogramming.orderservice.dto.OrderLineItemsDto;
+import com.rbprogramming.orderservice.dto.OrderRequest;
+import com.rbprogramming.orderservice.model.Order;
+import com.rbprogramming.orderservice.model.OrderLineItems;
+import com.rbprogramming.orderservice.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+    private final OrderRepository orderRepository;
+    public void placeOrder(OrderRequest orderRequest) {
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+        List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtosList()
+                .stream()
+                .map(this::mapToDto)
+                .toList();
+
+        order.setOrderLineItemsList(orderLineItems);
+
+        orderRepository.save(order);
+    }
+
+    private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
+        OrderLineItems orderLineItems = new OrderLineItems();
+        orderLineItems.setPrice(orderLineItemsDto.getPrice());
+        orderLineItems.setQuantity(orderLineItemsDto.getQuantity());
+        orderLineItems.setSkuCode(orderLineItemsDto.getSkuCode());
+
+        return orderLineItems;
+    }
+}
