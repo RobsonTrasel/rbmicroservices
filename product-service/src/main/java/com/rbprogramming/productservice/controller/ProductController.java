abbbb3491a9946b91420e341a2bf34511e0c5e71
RobsonTrasel <robson.trasel@gmail.com>
2023-10-25 21:51:32 -0300

    chore(controller): inject the product service on product controller

--- product-service/src/main/java/com/rbprogramming/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/rbprogramming/productservice/controller/ProductController.java
@@ -1,15 +1,22 @@
 package com.rbprogramming.productservice.controller;
 
 import com.rbprogramming.productservice.dto.ProductRequest;
+import com.rbprogramming.productservice.service.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/product")
+@RequiredArgsConstructor
 public class ProductController {
+
+    private final ProductService productService;
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void createProduct(@RequestBody ProductRequest productRequest ) {
-
+        productService.createProduct(productRequest);
     }
+
+
 }
