1a5de9da65aafa89a900f10bcf1c932a0b4908ad
RobsonTrasel <robson.trasel@gmail.com>
2023-10-25 21:36:07 -0300

    feat(repository): create a repository extending mongorepository

---
+++ product-service/src/main/java/com/rbprogramming/productservice/repository/ProductReposity.java
@@ -0,0 +1,7 @@
+package com.rbprogramming.productservice.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ProductReposity extends MongoRepository {
+
+}
