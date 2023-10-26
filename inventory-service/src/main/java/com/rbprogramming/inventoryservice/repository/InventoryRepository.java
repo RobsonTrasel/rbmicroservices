4fa4d4f3f6546ccdb4b7f080e0e0761dffb321f0
RobsonTrasel <robson.trasel@gmail.com>
2023-10-26 00:23:56 -0300

    feat(repository): create a repository for inventory-service
    Signed-off-by: RobsonTrasel <robson.trasel@gmail.com>

---
+++ inventory-service/src/main/java/com/rbprogramming/inventoryservice/repository/InventoryRepository.java
@@ -0,0 +1,10 @@
+package com.rbprogramming.inventoryservice.repository;
+
+import com.rbprogramming.inventoryservice.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    Optional<Inventory> findBySkuCode();
+}
