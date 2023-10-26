e8719b8dbab8cdd3d311a01d619418ad0a9941be
RobsonTrasel <robson.trasel@gmail.com>
2023-10-25 23:14:51 -0300

    chore(controller): add post mapping to setup the route
    Signed-off-by: RobsonTrasel <robson.trasel@gmail.com>

--- order-service/src/main/java/com/rbprogramming/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/rbprogramming/orderservice/controller/OrderController.java
@@ -1,4 +1,16 @@
 package com.rbprogramming.orderservice.controller;
 
+import com.rbprogramming.orderservice.dto.OrderRequest;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/product")
 public class OrderController {
+    @PostMapping
+    public String placeOrder(@RequestBody OrderRequest orderRequest) {
+        return "Order placed successfully";
+    }
 }
