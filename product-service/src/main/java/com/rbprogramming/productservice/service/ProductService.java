04cd983ddaa89a5110173289bd43af6a1bbc2151
RobsonTrasel <robson.trasel@gmail.com>
2023-10-25 22:01:36 -0300

    chore(service): add getAllProducts method and helper for it
    Signed-off-by: RobsonTrasel <robson.trasel@gmail.com>

--- product-service/src/main/java/com/rbprogramming/productservice/service/ProductService.java
+++ product-service/src/main/java/com/rbprogramming/productservice/service/ProductService.java
@@ -1,11 +1,14 @@
 package com.rbprogramming.productservice.service;
 
 import com.rbprogramming.productservice.dto.ProductRequest;
+import com.rbprogramming.productservice.dto.ProductResponse;
 import com.rbprogramming.productservice.model.Product;
 import com.rbprogramming.productservice.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -22,5 +25,20 @@ public class ProductService {
 
         productRepository.save(product);
         log.info("[PRODUCT] Product {} is saved", product.getId());
+    }
+
+    public List<ProductResponse> getAllProducts() {
+        List <Product> products = productRepository.findAll();
+
+        return products.stream().map(this::mapToProductResponse).toList();
+    }
+
+    private ProductResponse mapToProductResponse(Product product) {
+        return ProductResponse.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .build();
     }
 }
