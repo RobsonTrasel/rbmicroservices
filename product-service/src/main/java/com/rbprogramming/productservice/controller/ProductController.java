b286eac7b820733c592bf96bbcd9c6b81c9a4bb5
RobsonTrasel <robson.trasel@gmail.com>
2023-10-25 21:42:05 -0300

    chore(controller): add POST annotations for create a post function

--- product-service/src/main/java/com/rbprogramming/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/rbprogramming/productservice/controller/ProductController.java
@@ -1,9 +1,15 @@
 package com.rbprogramming.productservice.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.rbprogramming.productservice.dto.ProductRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/product")
 public class ProductController {
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody ProductRequest productRequest ) {
+
+    }
 }
