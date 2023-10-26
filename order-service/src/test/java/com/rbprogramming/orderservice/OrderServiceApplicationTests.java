6497545f3ad307035078a67c2548c722536011bc
RobsonTrasel <robson.trasel@gmail.com>
2023-10-25 22:54:57 -0300

    feat(service): create order service app
    Signed-off-by: RobsonTrasel <robson.trasel@gmail.com>

---
+++ order-service/src/test/java/com/rbprogramming/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rbprogramming.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
