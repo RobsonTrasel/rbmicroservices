0a4c35d741040a1b21a39ccc2493b0e3f0871bdc
RobsonTrasel <robson.trasel@gmail.com>
2023-10-28 11:53:55 -0300

    chore(controller): add log to debug questions
    Signed-off-by: RobsonTrasel <robson.trasel@gmail.com>

--- order-service/src/main/java/com/rbprogramming/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/rbprogramming/orderservice/controller/OrderController.java
@@ -1,20 +1,23 @@
 package com.rbprogramming.orderservice.controller;
 
-import com.rbprogramming.orderservice.dto.OrderRequest;
-import com.rbprogramming.orderservice.service.OrderService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+        import com.rbprogramming.orderservice.dto.OrderRequest;
+        import com.rbprogramming.orderservice.service.OrderService;
+        import lombok.RequiredArgsConstructor;
+        import lombok.extern.slf4j.Slf4j;
+        import org.springframework.http.HttpStatus;
+        import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/order")
 @RequiredArgsConstructor
+@Slf4j
 public class OrderController {
 
     private final OrderService orderService;
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public String placeOrder(@RequestBody OrderRequest orderRequest) {
+        log.info("The return of request: {}", orderRequest);
         orderService.placeOrder(orderRequest);
         return "Order placed successfully";
     }
