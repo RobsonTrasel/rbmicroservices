7e323c92960ac28bb86aacfcfc20332b8eeea6cb
RobsonTrasel <robson.trasel@gmail.com>
2023-10-28 12:50:31 -0300

    feat(server): enabled eureka server on main run applications
    Signed-off-by: RobsonTrasel <robson.trasel@gmail.com>

--- discovery-server/src/main/java/com/rbprogramming/discoveryserver/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/com/rbprogramming/discoveryserver/DiscoveryServerApplication.java
@@ -2,9 +2,10 @@ package com.rbprogramming.discoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-
+@EnableEurekaServer
 public class DiscoveryServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(DiscoveryServerApplication.class, args);
