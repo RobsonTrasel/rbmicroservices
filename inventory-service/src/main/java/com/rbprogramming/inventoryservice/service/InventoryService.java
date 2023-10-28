8dcdbcfcd0af3cc8cf7b79faf6368737976e6742
RobsonTrasel <robson.trasel@gmail.com>
2023-10-28 04:12:40 -0300

    chore(service): change for unique return method now is a list return
    Signed-off-by: RobsonTrasel <robson.trasel@gmail.com>

--- inventory-service/src/main/java/com/rbprogramming/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/rbprogramming/inventoryservice/service/InventoryService.java
@@ -5,12 +5,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class InventoryService {
     private final InventoryRepository inventoryRepository;
     @Transactional(readOnly = true)
-    public boolean isInStock(String skuCode) {
-        return inventoryRepository.findBySkuCode(skuCode).isPresent();
+    public boolean isInStock(List<String> skuCode) {
+        return inventoryRepository.findBySkuCodeIn(skuCode).isPresent();
     }
 }
