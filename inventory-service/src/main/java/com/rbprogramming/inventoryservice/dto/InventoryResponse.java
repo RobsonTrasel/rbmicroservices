fbb6b6a003cb6bf786021418266f17f062e6ebd3
RobsonTrasel <robson.trasel@gmail.com>
2023-10-28 04:15:37 -0300

    feat(dto): create a new package and new class for data transfer
    Signed-off-by: RobsonTrasel <robson.trasel@gmail.com>

---
+++ inventory-service/src/main/java/com/rbprogramming/inventoryservice/dto/InventoryResponse.java
@@ -0,0 +1,15 @@
+package com.rbprogramming.inventoryservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class InventoryResponse {
+    private String skuCode;
+    private boolean isInStock;
+}
