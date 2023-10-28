568660825201e9df94854fe3023187666aafbc27
RobsonTrasel <robson.trasel@gmail.com>
2023-10-28 04:04:08 -0300

    chore(service): put the webclient.get() inside order service to get request
    Signed-off-by: RobsonTrasel <robson.trasel@gmail.com>

--- order-service/src/main/java/com/rbprogramming/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/rbprogramming/orderservice/service/OrderService.java
@@ -8,6 +8,7 @@ import com.rbprogramming.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Collections;
 import java.util.List;
@@ -19,6 +20,7 @@ import java.util.UUID;
 @Transactional
 public class OrderService {
     private final OrderRepository orderRepository;
+    private final WebClient webClient;
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
@@ -32,6 +34,11 @@ public class OrderService {
 
         order.setOrderLineItemsList(orderLineItems);
         // Call inventory service and place the order if product is in stock
+        webClient.get()
+                .uri("http://localhost:8082/api/inventory")
+                .retrieve()
+                .bodyToMono(Boolean.class)
+                .block();
         orderRepository.save(order);
     }
 
