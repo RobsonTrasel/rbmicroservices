ae9f32d9846e40c40cfac8319e00bfc3195af30f
RobsonTrasel <robson.trasel@gmail.com>
2023-10-28 05:18:58 -0300

    chore(log): add log to log a bug for quantity
    Signed-off-by: RobsonTrasel <robson.trasel@gmail.com>

--- inventory-service/src/main/java/com/rbprogramming/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/com/rbprogramming/inventoryservice/controller/InventoryController.java
@@ -3,6 +3,7 @@ package com.rbprogramming.inventoryservice.controller;
 import com.rbprogramming.inventoryservice.dto.InventoryResponse;
 import com.rbprogramming.inventoryservice.service.InventoryService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,11 +12,13 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/inventory")
 @RequiredArgsConstructor
+@Slf4j
 public class InventoryController {
     private final InventoryService inventoryService;
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<InventoryResponse> isInStock(@RequestParam List<String> skuCode) {
+        log.info("Received inventory check request for skuCode: {}", skuCode);
         return inventoryService.isInStock(skuCode);
     }
 }
--- inventory-service/src/main/java/com/rbprogramming/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/rbprogramming/inventoryservice/service/InventoryService.java
@@ -3,6 +3,8 @@ package com.rbprogramming.inventoryservice.service;
 import com.rbprogramming.inventoryservice.dto.InventoryResponse;
 import com.rbprogramming.inventoryservice.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,11 +12,14 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class InventoryService {
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
+    @SneakyThrows
     public List<InventoryResponse> isInStock(List<String> skuCode) {
+        log.info("Checking Inventory");
         return inventoryRepository.findBySkuCodeIn(skuCode)
                 .stream()
                 .map(inventory ->
