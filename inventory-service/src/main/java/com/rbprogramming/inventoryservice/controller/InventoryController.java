244471ccce4460fe1d33c2841622aa5a60cf9ddb
RobsonTrasel <robson.trasel@gmail.com>
2023-10-28 04:12:09 -0300

    chore(controller): remove path params and add query params
    Signed-off-by: RobsonTrasel <robson.trasel@gmail.com>

--- inventory-service/src/main/java/com/rbprogramming/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/com/rbprogramming/inventoryservice/controller/InventoryController.java
@@ -5,14 +5,16 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/inventory")
 @RequiredArgsConstructor
 public class InventoryController {
     private final InventoryService inventoryService;
-    @GetMapping("/{sku-code}")
+    @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public boolean isInStock(@PathVariable("sku-code") String skuCode) {
+    public boolean isInStock(@RequestParam List<String>  skuCode) {
         return inventoryService.isInStock(skuCode);
     }
 }
